0942f1f872143932f3855769505013d84f58fb96
Sahil Agashe <sahil@scrambleapps.gmail.com>
2023-11-02 08:04:04 +0530

    Initial Commit

---
+++ My-WhatsApp/My_WhatsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_WhatsAppApp.swift
+//  My-WhatsApp
+//
+//  Created by SAHIL AMRUT AGASHE on 02/11/23.
+//
+
+import SwiftUI
+
+@main
+struct My_WhatsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
