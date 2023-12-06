4218fe157c43ba92fcf152403ec56c8612ac20b3
Sahil Agashe <sahil@scrambleapps.gmail.com>
2023-12-06 23:44:08 +0530

    Implemented loading messages in realtime functionality and added sorting feature to query( for fetching all documents in ascending order from firestore.)

--- My-WhatsApp/ViewModel/ChatViewModel.swift
+++ My-WhatsApp/ViewModel/ChatViewModel.swift
@@ -21,17 +21,36 @@ class ChatViewModel: ObservableObject {
         guard let currentUid = AuthViewModel.shared.userSession?.uid else { return }
         guard let chatPartnerId = user.id else { return }
         
-        let query = COLLECTION_MESSAGES.document(currentUid).collection(chatPartnerId)
-        query.getDocuments { (snapshot: QuerySnapshot?, error: Error?) in
-            guard let documents = snapshot?.documents else { return }
-            var messages = documents.compactMap({ try? $0.data(as: Message.self)})
+        let query = COLLECTION_MESSAGES
+            .document(currentUid)
+            .collection(chatPartnerId)
+            .order(by: "timestamp", descending: false)
+        
+//        // for fetching all documents, but not realtime-updating
+//        query.getDocuments { (snapshot: QuerySnapshot?, error: Error?) in
+//            guard let documents = snapshot?.documents else { return }
+//            var messages = documents.compactMap({ try? $0.data(as: Message.self)})
+//            
+//            for (index, message) in messages.enumerated() where message.fromId != currentUid {
+//                // setting user , for message not from current user
+//                messages[index].user = self.user
+//            }
+//            
+//            self.messages = messages
+//            print("DEBUG: messages are \(self.messages)")
+//        }
+        
+        // for fetching all documents with realtime updating whenever new document is added
+        query.addSnapshotListener { (snapshot: QuerySnapshot?, error: Error?) in
+            guard let changes = snapshot?.documentChanges.filter({ $0.type == .added }) else { return }
+            var messages = changes.compactMap({try? $0.document.data(as: Message.self)})
             
             for (index, message) in messages.enumerated() where message.fromId != currentUid {
                 // setting user , for message not from current user
                 messages[index].user = self.user
             }
             
-            self.messages = messages
+            self.messages.append(contentsOf: messages)
             print("DEBUG: messages are \(self.messages)")
         }
     }
